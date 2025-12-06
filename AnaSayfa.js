cbbeeff047b940aab43a674d021f2bc6c84851aa
furkanay7 <furkanay207@gmail.com>
2025-12-06 22:34:38 +0300

    Navigation yapısı eklendi

---
+++ AnaSayfa.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AnaSayfa = () => {
+  return (
+    <View>
+      <Text>AnaSayfa</Text>
+    </View>
+  )
+}
+
+export default AnaSayfa
+
+const styles = StyleSheet.create({})
