bc8bf1c54725aef74641a73d1cbd90f550a1d6dc
furkanay7 <furkanay207@gmail.com>
2025-12-07 22:44:22 +0300

    Tab navigator yapısı geliştirildi

--- App.js
+++ App.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import {NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import AnaSayfa from './AnaSayfa';
 import RaporSayfasi from './RaporSayfasi';
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
+      <Tab.Navigator>
+        <Tab.Screen
           name="Ana Sayfa"
           component={AnaSayfa}
         />
-        <Stack.Screen
+        <Tab.Screen
           name="Rapor Sayfası"
           component={RaporSayfasi}
         />
-      </Stack.Navigator>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
