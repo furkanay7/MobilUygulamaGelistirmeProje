f42514043ea8511b8496c3693726a486d7eaba7e
furkanay7 <furkanay207@gmail.com>
2025-12-10 21:36:45 +0300

    Rapor sayfası geliştirildi

--- RaporSayfasi.js
+++ RaporSayfasi.js
@@ -3,12 +3,23 @@ import React from 'react'
 
 const RaporSayfasi = () => {
   return (
-    <View>
-      <Text>RaporSayfasi</Text>
+    <View style={styles.container}>
+      <Text style={styles.baslik}>Bugün Odaklanma Süresi</Text>
+      <Text style={styles.baslik}>Tüm Zamanların Toplam Odaklanma Süresi</Text>
+      <Text style={styles.baslik}>Toplam Dikkat Dağınıklığı Sayısı</Text>
+      <Text style={styles.baslik}>Grafikler</Text>
     </View>
   )
 }
 
 export default RaporSayfasi
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    alignItems:'center',
+  },
+  baslik:{
+    fontSize:20,
+    margin : 10
+  }
+})
